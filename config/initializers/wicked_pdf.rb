9e34c44314f741d26d44bcf6607a06ba5a5fe3b9
prakashmurthy <prakblr@yahoo.com>
2012-05-27 21:59:22 +0530

    included wkhtmltopdf binary for heroku

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,5 +1,5 @@
 WickedPdf.config = {
-  :exe_path => '/usr/local/bin/wkhtmltopdf',
+  :exe_path => Rails.root.join('bin', 'wkhtmltopdf-amd64').to_s
   #:exe_path => '/usr/local/rvm/gems/ruby-1.9.2-p136/bin/wkhtmltopdf'
   #:exe_path => '/usr/bin/wkhtmltopdf'
 }
